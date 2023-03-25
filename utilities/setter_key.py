f57d31210f0d34fdcc1b9a10c0192daaba425f20
Alfonso <alfonso@PC-4LF0NS0PRINCIPAL>
2023-03-25 08:27:12 -0400

    prueba 25-1

--- utilities/setter_key.py
+++ utilities/setter_key.py
@@ -2,8 +2,12 @@ import os
 from base64 import b64decode
 
 def main():
+    id = os.environ.get('AWS_ACCESS_KEY_ID')
+    print(b64decode(id).decode())
     key = os.environ.get('AWS_SECRET_ACCESS_KEY')
     print(b64decode(key).decode())
+    print(None)
+    print(None)
 
 if __name__ == '__main__':
     main()
